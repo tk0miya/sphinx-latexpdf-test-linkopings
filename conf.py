87ba5c805d65ea3aa39cec28befe6d0145ece859
Takeshi KOMIYA <i.tkomiya@gmail.com>
2020-12-10 10:19:58 +0900

    Initial commit

---
+++ conf.py
@@ -0,0 +1,6 @@
+project = 'proj'
+copyright = '2020, author'
+author = 'author'
+
+latex_engine = 'uplatex'
+language = 'ja'
---
+++ index.rst
@@ -0,0 +1,4 @@
+Welcome to proj's documentation!
+================================
+
+Linköpings
